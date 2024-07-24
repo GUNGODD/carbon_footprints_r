6d51a4466c323efccc9e237e53b5e9bf49c47e41
GUNGODD <chauhanadityac01@gmail.com>
2024-07-24 17:42:44 +0530

    init

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,41 @@
 // Owenership
-#[path = "utils/project_carbon.rs"]
-mod project_carbon;
 
-#[path = "utils/calculator.rs"]
-mod calculator;
+// lib
+use std::{
+    io::{BufRead, BufReader},
+    net::{TcpListener, TcpStream},
+};
+
+// moduels/ paths
+// #[path = "utils/calculator.rs"]
+// mod calculator;
+// #[path = "utils/project_carbon.rs"]
+//mod project_carbon;
 fn main() {
-    calculator::calculator();
+    server_testing();
+}
+
+fn server_testing() {
+    // server tester
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    println!("Listening on http://127.0.0.1:7878");
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        handle_connection(stream);
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&mut stream);
+    let http_request: Vec<_> = buf_reader
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+
+    println!("Request: {http_request:#?}");
 }
 
 // Clone and copy
